dd01d8ce5bcbb32f4391545f73132cdfd534a810
Tensaibrew <raihanimamf@gmail.com>
2025-05-15 15:17:53 +0900

    messege

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
-// Toggle class active
+// Toggle class active hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 // ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Klik di luar sidebar untuk menghilangkan nav
+// Toggle class active search form
+const searchForm = document.querySelector(".search-form");
+const searchbox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchbox.focus();
+  e.preventDefault();
+};
+
+// Klik di luar elemen
 const hamburger = document.querySelector("#hamburger-menu");
+const searchbutton = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!searchbutton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
